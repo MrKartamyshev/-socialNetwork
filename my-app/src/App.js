d203e0a712831f6ea0f110b28794ae16f4505d45
Anton Kartamyshev <startucode@gmail.com>
2021-04-29 18:27:43 +0300

    structur start 11 less

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-wrapper'>
+      <heder className='header'>
+        <img src="https://www.logodesign.net/images/illustration-logo.png"></img>
+      </heder>
+      <nav className='nav'>
+        <div>
+         <a>Profile</a>
+        </div>
+        <div>
+         <a>Massages</a>
+        </div>
+        <div>
+          <a>News</a>
+        </div>
+        <div>
+          <a>Music</a>
+        </div>
+      </nav>
+      <div className='content'>
+        <div>
+        <img src='https://nashvancouver.com/wp-content/uploads/2017/03/10449154_961867807226456_598263691_n-e1489421545779-1024x556.jpg'></img>
+        </div>
+        <div>
+          ava + description
+        </div>
+        <div>
+          My post
+            <div>
+              New post
+            </div>
+        <div>
+          <div>
+          post 1 
+        </div>
+        <div>
+          post 2
+        </div>
+        </div>
+   </div>
     </div>
   );
-}
-
+};
 export default App;
